94019f5f086b70f062fd425e9ddb4ef7a3ee4bb4
Tim Hårek Andreassen <tim@harek.dev>
2022-03-22 23:39:07 +0100

    feat: If no config exists, prompt to create one
    Signed-off-by: Tim Hårek Andreassen <tim@harek.dev>

--- src/Config.ts
+++ src/Config.ts
@@ -8,11 +8,31 @@ export async function getConfig(): Promise<Config> {
 		return JSON.parse(await Deno.readTextFile(CONFIG_FILE_PATH));
 	} catch (error) {
 		console.error(error);
-		return {
-			coordinates: {
-				lat: 60.389444,
-				lng: 5.33,
-			},
-		};
+		console.warn('No config is present.');
+		const wantsNewConfig = confirm('Do you me to create one?');
+		if (wantsNewConfig) {
+			const lat = prompt('What is the latitude for your coordinates?');
+			const lng = prompt('What is the longitude for your coordinates?');
+
+			return generateConfigFile(lat, lng);
+		}
+
+		throw new Error('Missing config file');
 	}
+}
+
+async function generateConfigFile(
+	lat: string | null,
+	lng: string | null,
+): Promise<Config> {
+	const config: Config = {
+		coordinates: {
+			lat: Number(lat),
+			lng: Number(lng),
+		},
+	};
+
+	await Deno.writeTextFileSync(CONFIG_FILE_PATH, JSON.stringify(config));
+
+	return config;
 }
