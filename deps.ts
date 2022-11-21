2d4cb9b62ef5ef667dd19a7f21686b6ea0310a6c
Tim Hårek Andreassen <tim@harek.no>
2022-11-21 20:27:09 +0100

    chore(deps): Update dependencies
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- deps.ts
+++ deps.ts
@@ -1,9 +1,7 @@
-export { format } from 'https://deno.land/std@0.128.0/datetime/mod.ts';
-export { parse } from 'https://deno.land/std@0.145.0/flags/mod.ts';
-export { printHelp } from 'https://deno.land/x/deno_cli_help@1.0.1/mod.ts';
-export type { Options } from 'https://deno.land/x/deno_cli_help@1.0.1/mod.ts';
+export { format } from 'https://deno.land/std@0.165.0/datetime/mod.ts';
+export { parse } from 'https://deno.land/std@0.165.0/flags/mod.ts';
 export { Command } from 'https://deno.land/x/cliffy@v0.24.2/command/mod.ts';
-export { UpgradeCommand, GithubProvider } from "https://deno.land/x/cliffy@v0.24.2/command/upgrade/mod.ts";
+export { UpgradeCommand, GithubProvider } from "https://deno.land/x/cliffy@v0.25.4/command/upgrade/mod.ts";
 
 const HOME_PATH = Deno.env.get('HOME');
 export const CONFIG_FILE_PATH = `${HOME_PATH}/.config/yr/config.json`;
