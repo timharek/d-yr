cbf0c6bfb985a81c8c1862c258853920fc7121ae
Tim Hårek Andreassen <tim@harek.no>
2023-01-11 21:49:36 +0100

    refactor: Destructure variables for increased readability
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- src/yr.ts
+++ src/yr.ts
@@ -3,36 +3,37 @@ import { format as formatDate } from '../deps.ts';
 import { getNameFromCoordinates } from './nominatim.ts';
 
 /**
-  * Get the current weather as the hour closest from the time the request occured.
-  *
-  * @param weatherData Data from Yr.
-  * @param verbose Verbosity level
-  * @returns Current weather with based on `verbose`-level
-*/
+ * Get the current weather as the hour closest from the time the request occured.
+ *
+ * @param weatherData Data from Yr.
+ * @param verbose Verbosity level
+ * @returns Current weather with based on `verbose`-level
+ */
 async function getCurrentWeather(
   weatherData: Yr.IWeather,
   verbose: number,
 ) {
-  const units = weatherData.properties.meta.units;
-  const closestTimeseries: Yr.ITimeseries = getClosestTimeseries(
-    weatherData.properties.timeseries,
-  );
+  const {
+    properties: { meta: { units }, timeseries },
+    geometry: { coordinates },
+  } = weatherData;
+  const { data: { instant, next_1_hours: nextHour }, time: closestTime }:
+    Yr.ITimeseries = getClosestTimeseries(timeseries);
 
-  const lng = weatherData.geometry.coordinates[0];
-  const lat = weatherData.geometry.coordinates[1];
+  const lng = coordinates[0];
+  const lat = coordinates[1];
 
   const result = {
     location_name: await getNameFromCoordinates(lat, lng),
-    datetime: `${formatDate(new Date(closestTimeseries.time), 'HH:mm')}`,
-    symbol: closestTimeseries.data.next_1_hours.summary.symbol_code,
-    wind_speed:
-      `${closestTimeseries.data.instant.details.wind_speed} ${units.wind_speed}`,
-    temperature: `${closestTimeseries.data.instant.details.air_temperature} ${
+    datetime: `${formatDate(new Date(closestTime), 'HH:mm')}`,
+    symbol: nextHour.summary.symbol_code,
+    wind_speed: `${instant.details.wind_speed} ${units.wind_speed}`,
+    temperature: `${instant.details.air_temperature} ${
       units.air_temperature[0].toUpperCase()
     }`,
-    wind_direction: closestTimeseries.data.instant.details.wind_from_direction,
+    wind_direction: instant.details.wind_from_direction,
     rain:
-      `${closestTimeseries.data.next_1_hours.details.precipitation_amount} ${units.precipitation_amount}`,
+      `${nextHour.details.precipitation_amount} ${units.precipitation_amount}`,
   } as CLI.ITimeseriesSimple;
 
   return getVerboseMessage(result, verbose);
@@ -52,41 +53,44 @@ function getClosestTimeseries(
 }
 
 /**
-  * Get current upcoming forecast.
-  *
-  * @param weatherData Data from Yr.
-  * @param interval Numer of requested hours of forecast
-  * @param verbose Verbosity level
-  *
-  * @returns Upcoming forecast based on `interval`-level.
-  */
+ * Get current upcoming forecast.
+ *
+ * @param weatherData Data from Yr.
+ * @param interval Numer of requested hours of forecast
+ * @param verbose Verbosity level
+ *
+ * @returns Upcoming forecast based on `interval`-level.
+ */
 async function getForecastUpcoming(
   weatherData: Yr.IWeather,
   interval: number,
   verbose: number,
 ) {
-  const units = weatherData.properties.meta.units;
-  const closest = getClosestTimeseries(weatherData.properties.timeseries);
+  const {
+    properties: { meta: { units }, timeseries },
+    geometry: { coordinates },
+  } = weatherData;
+  const { time: closestTime } = getClosestTimeseries(timeseries);
 
   const result = weatherData.properties.timeseries.map((entry) => {
-    if (entry.time != closest.time && entry?.data?.next_1_hours) {
+    const { data: { instant, next_1_hours: nextHour }, time } = entry;
+    if (time != closestTime && nextHour) {
       return {
-        datetime: `${formatDate(new Date(entry.time), 'yyyy-MM-dd HH:mm')}`,
-        symbol: entry.data.next_1_hours.summary.symbol_code,
-        wind_speed:
-          `${entry.data.instant.details.wind_speed} ${units.wind_speed}`,
-        wind_direction: entry.data.instant.details.wind_from_direction,
-        temperature: `${entry.data.instant.details.air_temperature} ${
+        datetime: `${formatDate(new Date(time), 'yyyy-MM-dd HH:mm')}`,
+        symbol: nextHour.summary.symbol_code,
+        wind_speed: `${instant.details.wind_speed} ${units.wind_speed}`,
+        wind_direction: instant.details.wind_from_direction,
+        temperature: `${instant.details.air_temperature} ${
           units.air_temperature[0].toUpperCase()
         }`,
         rain:
-          `${entry.data.next_1_hours.details.precipitation_amount} ${units.precipitation_amount}`,
+          `${nextHour.details.precipitation_amount} ${units.precipitation_amount}`,
       } as CLI.ITimeseriesSimple;
     }
   });
 
-  const lng = weatherData.geometry.coordinates[0];
-  const lat = weatherData.geometry.coordinates[1];
+  const lng = coordinates[0];
+  const lat = coordinates[1];
 
   const array = interval
     ? cleanForecast(result).slice(0, interval)
@@ -128,8 +132,8 @@ function cleanForecast(input: Array<unknown>) {
 }
 
 /**
-  * @returns Yr.no's request URL
-  */
+ * @returns Yr.no's request URL
+ */
 function getUrl(lat: number, lng: number) {
   const yrUrl =
     `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${lng}`;
@@ -140,4 +144,4 @@ export const Yr = {
   current: getCurrentWeather,
   forecast: getForecastUpcoming,
   getUrl,
-}
+};
