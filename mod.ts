fdd0b2f18933dd56422dbf925add1e0cd60bd897
Tim Hårek Andreassen <tim@harek.no>
2023-01-29 21:20:19 +0100

    docs: Add docs to `_fetch`
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- mod.ts
+++ mod.ts
@@ -1,6 +1,12 @@
 export { Nominatim } from './src/nominatim.ts';
 export { Yr } from './src/yr.ts';
 
+/**
+ * Shared fetch-function for simple GET-requests.
+ *
+ * @param url The URL to send GET-request to
+ * @returns Result from GET-request.
+ */
 export async function _fetch(url: string | URL) {
   const result = await fetch(url, {
     method: 'GET',
