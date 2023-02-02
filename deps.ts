b8d390e3e96b799e233a4d23e5e9c4f13fd7ffe9
Tim Hårek Andreassen <tim@harek.no>
2023-02-02 21:34:11 +0100

    fix: Remove import_map
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- deps.ts
+++ deps.ts
@@ -1,4 +1,7 @@
-export { format } from 'std/datetime/mod.ts';
-export { parse } from 'std/flags/mod.ts';
-export { Command } from 'cliffy/command/mod.ts';
-export { GithubProvider, UpgradeCommand } from 'cliffy/command/upgrade/mod.ts';
+export { format } from 'https://deno.land/std@0.174.0/datetime/mod.ts';
+export { parse } from 'https://deno.land/std@0.174.0/flags/mod.ts';
+export { Command } from 'https://deno.land/x/cliffy@v0.25.7/command/mod.ts';
+export {
+  GithubProvider,
+  UpgradeCommand,
+} from 'https://deno.land/x/cliffy@v0.25.7/command/upgrade/mod.ts';
--- src/cli.ts
+++ src/cli.ts
@@ -16,7 +16,7 @@ const forecastCmd = new Command()
 
 await new Command()
   .name('yr')
-  .version('v1.3.3')
+  .version('v1.3.4')
   .description('Get weather data from Yr using Deno.')
   .meta('Author', 'Tim Hårek Andreassen <tim@harek.no>')
   .meta('Source', 'https://github.com/timharek/d-yr')
