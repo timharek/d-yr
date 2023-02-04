867538732e2f1206353130a9666c6f3935af0bab
Tim Hårek Andreassen <tim@harek.no>
2023-02-04 14:15:31 +0100

    chore: Bump version
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- src/cli.ts
+++ src/cli.ts
@@ -18,7 +18,7 @@ const forecastCmd = new Command()
 
 await new Command()
   .name('yr')
-  .version('v1.3.5')
+  .version('v1.4.0')
   .description('Get weather data from Yr using Deno.')
   .meta('Author', 'Tim Hårek Andreassen <tim@harek.no>')
   .meta('Source', 'https://github.com/timharek/d-yr')
