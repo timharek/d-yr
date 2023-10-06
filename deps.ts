4ccfb85b8a776ac4a9a59c0376460cb4e031ad4a
Tim Hårek Andreassen <tim@harek.no>
2023-10-06 20:14:03 +0200

    chore(deps): Update deps
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { format } from 'https://deno.land/std@0.176.0/datetime/mod.ts';
-export { parse } from 'https://deno.land/std@0.176.0/flags/mod.ts';
-export * as Colors from 'https://deno.land/std@0.176.0/fmt/colors.ts';
-export { Command } from 'https://deno.land/x/cliffy@v0.25.7/command/mod.ts';
+export { format } from 'https://deno.land/std@0.203.0/datetime/mod.ts';
+export { parse } from 'https://deno.land/std@0.203.0/flags/mod.ts';
+export * as Colors from 'https://deno.land/std@0.203.0/fmt/colors.ts';
+export { Command } from 'https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts';
